977bd6941b3342d2cb47995ad50c674aed56ba01
phenphichacho4740-droid <phenphicha.cho4740@ndlp.go.th>
2026-08-18 11:14:04 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 document.addEventListener('DOMContentLoaded',function(){
+  document.getElementById('year').innerHTML = new Date().getFullYear();
   console.log('Portfolio ของ PENPICHA พร้อมแล้ว!');
 });
